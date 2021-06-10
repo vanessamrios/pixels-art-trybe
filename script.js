224c89e3fd23d8b6ec8d27d642a55b038d939368
Vanessa <vanessamrios@gmail.com>
2021-06-10 16:17:53 -0300

    atende o requisito 7

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function blackColorSelected (){
 }
     
 blackColorSelected();
+
+let black = document.getElementById("black");
+let yellow = document.getElementById("yellow");
+let green = document.getElementById("green");
+let blue = document.getElementById("blue");
+
+black.addEventListener("click", addClassSelected);
+yellow.addEventListener("click", addClassSelected);
+green.addEventListener("click", addClassSelected);
+blue.addEventListener("click", addClassSelected);
+
+function addClassSelected (event){
+    let colorSelected = document.querySelector(".selected")
+    colorSelected.classList.remove("selected");
+    event.target.classList.add("selected");
+}
