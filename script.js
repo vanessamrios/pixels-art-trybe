847f22871bd630557dc2249c280428390c7d1fa2
Vanessa <vanessamrios@gmail.com>
2021-06-11 11:17:51 -0300

    atende o requisito 9 e 100% dos obrigatorios.

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-function blackColorSelected (){
-    let colorBlack = document.getElementById("black");
-    colorBlack.classList.add("selected");
+function blackColorSelected() {
+  let colorBlack = document.getElementById('black');
+  colorBlack.classList.add('selected');
 }
-    
+
 blackColorSelected();
 
-let black = document.getElementById("black");
-let yellow = document.getElementById("yellow");
-let green = document.getElementById("green");
-let blue = document.getElementById("blue");
+let black = document.getElementById('black');
+let yellow = document.getElementById('yellow');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
 
-black.addEventListener("click", addClassSelected);
-yellow.addEventListener("click", addClassSelected);
-green.addEventListener("click", addClassSelected);
-blue.addEventListener("click", addClassSelected);
+black.addEventListener('click', addClassSelected);
+yellow.addEventListener('click', addClassSelected);
+green.addEventListener('click', addClassSelected);
+blue.addEventListener('click', addClassSelected);
 
-function addClassSelected (event){
-    let colorSelected = document.querySelector(".selected")
-    colorSelected.classList.remove("selected");
-    event.target.classList.add("selected");
+function addClassSelected(event) {
+  let colorSelected = document.querySelector('.selected');
+  colorSelected.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
-function createArt (){
-    let pixels = document.getElementsByClassName("pixel");
-    
-    
-    for(let pixel of pixels) {
-        pixel.addEventListener("click", changeColor);
-        function changeColor(event) {
-        let selected = document.getElementsByClassName("selected");
-        event.target.style.backgroundColor = selected[0].id;
-        }
+function createArt() {
+  let pixels = document.getElementsByClassName('pixel');
+
+  for (let pixel of pixels) {
+    pixel.addEventListener('click', changeColor);
+    function changeColor(event) {
+      let selected = document.getElementsByClassName('selected');
+      event.target.style.backgroundColor = selected[0].id;
     }
+  }
 }
 createArt();
+
+function clearBoard() {
+  let clearButton = document.getElementById('clear-board');
+  clearButton.addEventListener('click', clear);
+  function clear() {
+    let pixels = document.getElementsByClassName('pixel');
+    for (let pixel of pixels) {
+      if (pixel.style.backgroundColor != 'white'){
+        pixel.style.backgroundColor = 'white';
+      }
+    }
+  }
+}
+clearBoard();
